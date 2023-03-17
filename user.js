093d8dd623098254f6f7f4bc7a74296c17997e1d
Junbeom Chun <92chuck@gmail.com>
2023-03-16 21:34:56 -0400

    first commit

---
+++ user.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const PORT = 3000;
+
+app.get('/users', async (req, res) => {
+  try {
+    const { data } = await axios.get(
+      'https://jsonplaceholder.typicode.com/users'
+    );
+    if (!data.length) throw new Error('No users found!');
+    res.status(200).send(data);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on http://localhost:${PORT}`);
+});
